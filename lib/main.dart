a9f4ba3f9651cae9320bd411ec17712826f9e944
kumaranbu <kumaranbu283@gmail.com>
2023-09-03 21:41:22 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+// ignore: unnecessary_new
+void main()=>runApp(new MaterialApp(
+  home: new MyApp(),
+));
+
+
+
+
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build (BuildContext context){
+    return 
+  }
+}
+
+
